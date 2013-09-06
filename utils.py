48b7d12938a99452df860ca5ce9bac7b7c9e671a
Danny Wynne <wynne100@gmail.com>
2013-09-05 20:49:28 -0700

    added widget types

--- utils.py
+++ utils.py
@@ -8,20 +8,22 @@ def get_ui_gvars():
 	#only get gvars that are qt related
 	gvars = []
 	for g in [x for x in sorted(mel.eval('env')) if x.find("$g")>-1]:
+		widget_type = None
 		try:
 			var_type = mel.eval('whatIs "%s"'%g)
 			if not var_type == "string variable":
 				raise TypeError
-				
+
 			tmp = mel.eval('string $temp = %s;'%g)
 
-			if not type(tmp) == type(u''):
+			if tmp is None:
 				raise TypeError
 
-			target_widget = qt.toQtObject(tmp)
+			target_widget = qt.pathToWidget(tmp)
 			if type(target_widget) == type(None):
 				raise ValueError
+			widget_type = type(target_widget)
 		except:
 			continue
-		gvars.append(g)
+		gvars.append([g, widget_type.__name__])
 	return gvars
--- window.py
+++ window.py
@@ -16,9 +16,10 @@ class MayaQtExplorer(baseclass):
 		super(MayaQtExplorer, self).__init__(parent)
 		self.maya_window = parent
 		
-		self.setUnmanagedWidgets([self.gvars_list, self.info_te])
+		self.setUnmanagedWidgets([self.gvars_list, self.info_te, self.current_widget_path])
 
 		self.gvars = explorer_utils.get_ui_gvars()
+		self.gvars = [" - ".join(x) for x in self.gvars]
 		self.gvars_list.clear()
 		self.gvars_list.addItems(self.gvars)
 
@@ -27,15 +28,28 @@ class MayaQtExplorer(baseclass):
 		self.gvars_list.itemDoubleClicked.connect(self.gvar_double_clicked)
 		self.parent_list.itemDoubleClicked.connect(self.ui_path_list_double_clicked)
 		self.child_list.itemDoubleClicked.connect(self.ui_path_list_double_clicked)
+		self.search_gvars.textChanged.connect(self.search_changed)
 
+		self.band = None
 
-		self._band = None
+	def search_changed(self):
+		search = str(self.search_gvars.text()).lower()
+		if len(search) < 3:
+			self.gvars_list.clear()
+			self.gvars_list.addItems(self.gvars)
+			return
+		_tmp_gvars = []
+		for g in self.gvars:
+			if g.lower().find(search) > -1:
+				_tmp_gvars.append(g)
+		self.gvars_list.clear()
+		self.gvars_list.addItems(_tmp_gvars)
 
 	def ui_path_list_double_clicked(self, *args):
 		ui_path = args[0].text()
 		try:
-			widget = qt.toQtObject(ui_path)
-			if type(widget) == type(None):
+			widget = qt.pathToWidget(ui_path.partition(" - ")[2])
+			if widget is None:
 				raise TypeError
 		except:
 			return
@@ -44,8 +58,8 @@ class MayaQtExplorer(baseclass):
 	def gvar_double_clicked(self, *args):
 		gvar = args[0].text()
 		try:
-			tmp = mel.eval('string $temp = %s;'%gvar)
-			widget = qt.toQtObject(tmp)
+			tmp = mel.eval('string $temp = %s;'%gvar.partition(" - ")[0])
+			widget = qt.pathToWidget(tmp)
 		except:
 			print "probably not a QObject"
 			return
@@ -53,18 +67,21 @@ class MayaQtExplorer(baseclass):
 
 	def set_current_widget(self, widget):
 
-		ui_path = qt.widgetToMayaName(widget)
-		self.current_widget_path.setText(ui_path)
+		ui_path = qt.widgetToPath(widget)
+		widget_type = type(widget).__name__
+		self.current_widget_path.setText(widget_type + " - " + ui_path)
 
 		children = widget.children()
 		self.child_list.clear()
 		for c in children:
-			ui_path = qt.widgetToMayaName(c)
-			self.child_list.addItem(ui_path)
+			c_type = type(c).__name__
+			ui_path = qt.widgetToPath(c)
+			self.child_list.addItem(c_type + " - " + ui_path)
 
 		parent = widget.parent()
 		self.parent_list.clear()
-		self.parent_list.addItem(qt.widgetToMayaName(parent))
+		parent_type = type(parent).__name__
+		self.parent_list.addItem(parent_type + " - " + qt.widgetToPath(parent))
 
 		self.rubber_band_widget(widget)
 
@@ -73,11 +90,10 @@ class MayaQtExplorer(baseclass):
 			self.band.hide();
 		except:
 			pass
-
-		self.band = QtGui.QRubberBand(QtGui.QRubberBand.Rectangle, self.maya_window)
+		if self.band is None:
+			self.band = QtGui.QRubberBand(QtGui.QRubberBand.Rectangle, self.maya_window)
 		rect = widget.geometry()
 		pnt = widget.mapTo(self.maya_window, widget.geometry().topLeft())
 		rect.moveTo(pnt)
 		self.band.setGeometry(rect)
-
 		self.band.show()
